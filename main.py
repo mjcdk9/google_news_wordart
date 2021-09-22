8d9f4e3d3407e5da82287acda75cc4b1334dbd88
Mark Chafin <markchafin@Marks-MacBook-Pro.local>
2021-09-22 09:36:34 -0500

    appends articles and creates a new file for every day

--- main.py
+++ main.py
@@ -2,39 +2,41 @@ import json
 
 import rainbow as rainbow
 from requests_html import HTMLSession
-
+from datetime import date
+import os
+path = 'text_files'
+today = date.today()
 # open a (new) file to write
-outF = open("data.txt", "w")
+createFile = open(os.path.join(path, f'{today}.txt'), "w")
+createFile.close()
+
+# open a read and append version so I can compare and add new headlines
+readFile = open(os.path.join(path, f'{today}.txt'), "r")
+appendFile = open(os.path.join(path, f'{today}.txt'), "a")
+
+readFile =readFile.read()
+# print(readFile)
 
 url = 'https://news.google.com/rss/'
 # url = 'https://news.google.com/rss/search?q=political'
-
+list = []
 s = HTMLSession()
 
 r = s.get(url)
 
 for title in r.html.find('title'):
     title = str(title.text)
-    # print(title)
-    outF.write(title)
-    outF.write("\n")
-outF.close()
+    list.append(title)
+    if title not in readFile:
+        appendFile.write(f'{title}\n')
 
 
 
-my_file = open("data.txt", "r")
-content = my_file.read()
-content_list = content.split("\n")
-# content_list = my_file.readlines()
-# my_file.close()
-print(content_list)
+#
+# from PIL import Image
+# from pythonWordArt import pyWordArt
+# w = pyWordArt()
+# w.WordArt(str(my_file), w.Styles[mystyle], "100")
+# pil_im = Image.open(w.toBufferIO())
+# pil_im.show()
 
-
-
-
-from PIL import Image
-from pythonWordArt import pyWordArt
-w = pyWordArt()
-w.WordArt(str(my_file), w.Styles[mystyle], "100")
-pil_im = Image.open(w.toBufferIO())
-pil_im.show()
